db18c71b35415adf694034a61c97aca3c79d48e7
felipe16rojas <fr17885@gmail.com>
2026-05-14 21:20:04 -0500

    carta_amor

---
+++ js/script.js
@@ -0,0 +1,93 @@
+let timeLeft = 20;
+
+const countdown = document.getElementById("countdown");
+const envelope = document.getElementById("envelope");
+const text = document.getElementById("text");
+
+const openSound = document.getElementById("openSound");
+const music = document.getElementById("music");
+
+const message =
+"No hay que esperar fechas importantes para este tipo de detalles. Esta carta es para decirte que te amo demasiado. Estoy profundamente agradecido con Dios, con la vida y contigo. Gracias por llegar a mi vida y por quedarte conmigo incluso en la distancia. Te amo más de lo que las palabras pueden decir 💕";
+
+/* EFECTO MAQUINA DE ESCRIBIR */
+function typeWriter(i = 0) {
+
+    if (i < message.length) {
+
+        text.innerHTML += message.charAt(i);
+
+        setTimeout(() => {
+            typeWriter(i + 1);
+        }, 45);
+    }
+}
+
+/* CORAZONES */
+function createHeart() {
+
+    const heart = document.createElement("div");
+
+    heart.classList.add("heart");
+
+    heart.innerHTML = "💕";
+
+    heart.style.left = Math.random() * 100 + "vw";
+
+    heart.style.fontSize = (Math.random() * 20 + 10) + "px";
+
+    document.querySelector(".hearts").appendChild(heart);
+
+    setTimeout(() => {
+        heart.remove();
+    }, 4000);
+}
+
+/* COUNTDOWN PRINCIPAL */
+function start() {
+
+    countdown.innerHTML = timeLeft + "s";
+
+    /* MUSICA SUAVE DESDE EL INICIO */
+    if (timeLeft == 20) {
+
+        music.volume = 0.25;
+
+        music.play();
+    }
+
+    /* ABRIR CARTA */
+    if (timeLeft <= 0) {
+
+        clearInterval(timer);
+
+        /* desaparecer contador */
+        countdown.style.opacity = "0";
+
+        setTimeout(() => {
+
+            /* abrir sobre */
+            envelope.classList.add("open");
+
+            /* sonido apertura */
+            openSound.play();
+
+            /* esperar para mostrar letras */
+            setTimeout(() => {
+
+                /* escribir mensaje */
+                typeWriter();
+
+                /* corazones */
+                setInterval(createHeart, 250);
+
+            }, 1500);
+
+        }, 700);
+    }
+
+    timeLeft--;
+}
+
+/* INICIAR */
+let timer = setInterval(start, 1000);
